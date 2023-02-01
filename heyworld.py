f9a9bffa0c6b660b890a70b98f6b451a852f0558
Jason Fuhrer <jf.33@hotmail.com>
2023-01-31 22:42:16 -0500

    variable types

--- heyworld.py
+++ heyworld.py
@@ -2,3 +2,10 @@ msg = "hey world"
 
 print("original= " + msg)
 print("caps'd= " + msg.capitalize())
+
+stringTest = "hey it's a string"
+print(stringTest)
+
+aFloat = 8.2
+floatStr = "This here be a float, matey: "
+print(floatStr, aFloat)
